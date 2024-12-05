21b9c2cd53b5f95d89dbd643fab49390035b6529
LochlainnB <golden.github@brindley.it>
2024-12-05 14:19:42 +0000

    Finished Day 5 Part 1.

--- AoC2024/Day05.cpp
+++ AoC2024/Day05.cpp
@@ -11,18 +11,55 @@
 
 void solution(std::string file) {
 	std::string rawInput = Utils::loadFile(file);
-	// Use lines for 1D vector
-	std::vector<std::string> lines = Utils::split(rawInput, "\n");
-	// Use input for 2D vector
-	std::vector<std::vector<std::string>> input;
-	for (int i = 0; i < lines.size(); i++) {
-		input.push_back(Utils::split(lines[i], " "));
+	// Split into the two input types
+	std::vector<std::string> inputs = Utils::split(rawInput, "\n\n");
+
+	std::vector<std::string> ruleLines = Utils::split(inputs[0], "\n");
+	std::vector<std::string> updateLines = Utils::split(inputs[1], "\n");
+	
+	std::vector<std::vector<std::string>> rules;
+	for (int i = 0; i < ruleLines.size(); i++) {
+		rules.push_back(Utils::split(ruleLines[i], "|"));
+	}
+	std::vector<std::vector<std::string>> updates;
+	for (int i = 0; i < updateLines.size(); i++) {
+		updates.push_back(Utils::split(updateLines[i], ","));
 	}
 
 	// Part 1
+	std::unordered_map<std::string, std::set<std::string>> keyBeforeValue;
+	std::unordered_map<std::string, std::set<std::string>> keyAfterValue;
+	for (int i = 0; i < rules.size(); i++) {
+		keyBeforeValue[rules[i][0]].insert(rules[i][1]);
+		keyAfterValue[rules[i][1]].insert(rules[i][0]);
+	}
 
-	std::cout << "Part 1: " <<  << "\n";
-	Utils::copy();
+	int total = 0;
+	for (int i = 0; i < updates.size(); i++) {
+		bool valid = true;
+		for (int j = 0; j < updates[i].size(); j++) {
+			// Look backwards
+			for (int k = j - 1; k >= 0; k--) {
+				if (keyBeforeValue[updates[i][j]].count(updates[i][k])) {
+					valid = false;
+					break;
+				}
+			}
+			// Look fowards
+			for (int k = j + 1; k < updates[i].size(); k++) {
+				if (keyAfterValue[updates[i][j]].count(updates[i][k])) {
+					valid = false;
+					break;
+				}
+			}
+		}
+		if (valid) {
+			total += std::stoi(updates[i][updates[i].size() / 2]);
+		}
+	}
+
+	std::cout << "Part 1: " << total << "\n";
+	Utils::copy(total);
 
 	// Part 2
 
