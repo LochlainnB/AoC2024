1e5c3e585b9c2e85aa7f5b7d1522e11fee70496a
LochlainnB <golden.github@brindley.it>
2024-12-05 00:02:22 +0000

    Started Day 4.

---
+++ AoC2024/Day04.cpp
@@ -0,0 +1,38 @@
+#pragma once
+#include "Utils.h"
+#include <algorithm>
+#include <iostream>
+#include <array>
+#include <stack>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <queue>
+
+void solution(std::string file) {
+	std::string rawInput = Utils::loadFile(file);
+	// Use lines for 1D vector
+	std::vector<std::string> lines = Utils::split(rawInput, "\n");
+	// Use input for 2D vector
+	std::vector<std::vector<std::string>> input;
+	for (int i = 0; i < lines.size(); i++) {
+		input.push_back(Utils::split(lines[i], " "));
+	}
+
+	// Part 1
+	
+	std::cout << "Part 1: " <<  << "\n";
+	Utils::copy();
+
+	// Part 2
+
+	//std::cout << "Part 2: " <<  << "\n";
+	//Utils::copy();
+}
+
+int main() {
+	std::cout << "Test data results:\n";
+	solution("Test.txt");
+	std::cout << "Puzzle input results:\n";
+	solution("Input.txt");
+}
