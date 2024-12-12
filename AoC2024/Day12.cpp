6f3b8aef08ab53673892c60acc3bdc227421b329
LochlainnB <golden.github@brindley.it>
2024-12-12 16:53:03 +0000

    Finished Day 12 Part 1.

--- AoC2024/Day12.cpp
+++ AoC2024/Day12.cpp
@@ -9,6 +9,17 @@
 #include <set>
 #include <queue>
 
+struct pairHash {
+	size_t operator()(const std::pair<int, int>& key) const {
+		return static_cast<long long>(key.first) << 32 & key.second;
+	}
+};
+
+struct Region {
+	int plots;
+	int fences;
+};
+
 void solution(std::string file) {
 	std::string rawInput = Utils::loadFile(file);
 	// Use lines for 1D vector
@@ -20,9 +31,66 @@ void solution(std::string file) {
 	}
 
 	// Part 1
+	std::set<Region*> regions;
+	std::unordered_map<std::pair<int, int>, Region*, pairHash> regionAtPosition;
+	for (int y = 0; y < lines.size(); y++) {
+		for (int x = 0; x < lines[y].size(); x++) {
+			std::vector<std::pair<int, int>> neighbors(4, {x, y});
+			neighbors[0].first++;
+			neighbors[1].first--;
+			neighbors[2].second++;
+			neighbors[3].second--;
+			for (int i = 0; i < neighbors.size(); i++) {
+				if (neighbors[i].first < 0
+				|| neighbors[i].second < 0
+				|| neighbors[i].first >= lines[y].size()
+				|| neighbors[i].second >= lines.size()
+				|| lines[y][x] != lines[neighbors[i].second][neighbors[i].first]) {
+					neighbors.erase(neighbors.begin() + i);
+					i--;
+				}
+			}
+			Region* region = nullptr;
+			std::vector<Region*> neighborRegions;
+			for (int i = 0; i < neighbors.size(); i++) {
+				if (regionAtPosition.count(neighbors[i])) {
+					neighborRegions.push_back(regionAtPosition[neighbors[i]]);
+				}
+			}
+			// Handle multiple Regions that are actually part of the same region
+			for (int j = 1; j < neighborRegions.size(); j++) {
+				if (neighborRegions[0] != neighborRegions[j]) {
+					neighborRegions[0]->fences += neighborRegions[j]->fences;
+					neighborRegions[0]->plots += neighborRegions[j]->plots;
+					for (auto record : regionAtPosition) {
+						if (record.second == neighborRegions[j]) {
+							regionAtPosition[record.first] = neighborRegions[0];
+						}
+					}
+					regions.erase(neighborRegions[j]);
+					delete neighborRegions[j];
+				}
+			}
+			if (neighborRegions.size() > 0) {
+				region = neighborRegions[0];
+			}
+			if (!region) {
+				region = new Region();
+				regions.insert(region);
+			}
+			region->plots++;
+			region->fences += 4 - neighbors.size();
+			regionAtPosition[{x, y}] = region;
+		}
+	}
 
-	std::cout << "Part 1: " <<  << "\n";
-	Utils::copy();
+	long long total = 0;
+	for (auto region : regions) {
+		total += region->plots * region->fences;
+	}
+
+	std::cout << "Part 1: " << total << "\n";
+	Utils::copy(total);
 
 	// Part 2
 
