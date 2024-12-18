7837c919c561f7577ff54db29e51c3acf2968a59
LochlainnB <golden.github@brindley.it>
2024-12-18 17:18:37 +0000

    Finished Day 18 Part 1.

--- AoC2024/Day18.cpp
+++ AoC2024/Day18.cpp
@@ -9,6 +9,60 @@
 #include <set>
 #include <queue>
 
+struct Node {
+	Utils::Vector2ll position;
+	long long cost;
+	long long h;
+};
+
+struct Comp {
+	bool operator()(Node* lhs, Node* rhs) {
+		return lhs->cost + lhs->h > rhs->cost + rhs->h;
+	}
+};
+
+long long aStar(std::vector<std::vector<Node*>>& graph) {
+	Utils::Vector2ll startPos = { 0, 0 };
+	Utils::Vector2ll endPos = Utils::Vector2ll(graph[0].size() - 1, graph.size() - 1);
+
+	std::priority_queue<Node*, std::vector<Node*>, Comp> openSet;
+	Node* start = graph[startPos.y][startPos.x];
+	start->cost = 0;
+	openSet.push(start);
+
+	long long result = -1;
+
+	while (!openSet.empty()) {
+		Node* node = openSet.top();
+		openSet.pop();
+		std::vector<Node*> candidates;
+
+		if (node->position.x > 0) {
+			candidates.push_back(graph[node->position.y][node->position.x - 1]);
+		}
+		if (node->position.x < graph[0].size() - 1) {
+			candidates.push_back(graph[node->position.y][node->position.x + 1]);
+		}
+		if (node->position.y > 0) {
+			candidates.push_back(graph[node->position.y - 1][node->position.x]);
+		}
+		if (node->position.y < graph.size() - 1) {
+			candidates.push_back(graph[node->position.y + 1][node->position.x]);
+		}
+
+		for (int i = 0; i < candidates.size(); i++) {
+			if (candidates[i] != nullptr && candidates[i]->cost > node->cost + 1) {
+				candidates[i]->cost = node->cost + 1;
+				openSet.push(candidates[i]);
+				if (candidates[i]->position == endPos) {
+					return candidates[i]->cost;
+				}
+			}
+		}
+	}
+	return -1;
+}
+
 void solution(std::string file) {
 	std::string rawInput = Utils::loadFile(file);
 	// Use lines for 1D vector
@@ -16,13 +70,30 @@ void solution(std::string file) {
 	// Use input for 2D vector
 	std::vector<std::vector<std::string>> input;
 	for (int i = 0; i < lines.size(); i++) {
-		input.push_back(Utils::split(lines[i], " "));
+		input.push_back(Utils::split(lines[i], ","));
 	}
 
 	// Part 1
+	std::set<Utils::Vector2ll> corruption;
+	for (int i = 0; i < (file == "Test.txt" ? 12 : 1024); i++) {
+		corruption.insert({ std::stoi(input[i][0]), std::stoi(input[i][1]) });
+	}
 
-	std::cout << "Part 1: " <<  << "\n";
-	Utils::copy();
+	int size = file == "Test.txt" ? 7 : 71;
+	Utils::Vector2ll endPos = { size - 1, size - 1 };
+	std::vector<std::vector<Node*>> graph(size, std::vector<Node*>(size, nullptr));
+	for (int y = 0; y < graph.size(); y++) {
+		for (int x = 0; x < graph[0].size(); x++) {
+			if (corruption.count({ x, y }) == 0) {
+				graph[y][x] = new Node{ {x, y}, LLONG_MAX, (endPos - Utils::Vector2ll(x, y)).getManhattan() };
+			}
+		}
+	}
+
+	long long steps = aStar(graph);
+
+	std::cout << "Part 1: " << steps << "\n";
+	Utils::copy(steps);
 
 	// Part 2
 
