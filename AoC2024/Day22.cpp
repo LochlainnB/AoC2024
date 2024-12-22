10e6f0186dbd551c8afdec6e23b8655a82c2c0db
LochlainnB <golden.github@brindley.it>
2024-12-22 21:50:26 +0000

    Finished Day 22 Part 2.

--- AoC2024/Day22.cpp
+++ AoC2024/Day22.cpp
@@ -9,6 +9,23 @@
 #include <set>
 #include <queue>
 
+struct Sequence {
+	long long first, second, third, fourth;
+
+	bool operator<(const Sequence& other) const {
+		if (first != other.first) return first < other.first;
+		if (second != other.second) return second < other.second;
+		if (third != other.third) return third < other.third;
+		return fourth < other.fourth;
+	}
+};
+
+struct SequenceHash {
+	size_t operator()(const Sequence& seq) {
+		return seq.first ^ seq.second << 1 ^ seq.third << 2 ^ seq.fourth << 3;
+	}
+};
+
 long long advanceSecret(long long secret) {
 	secret = (secret ^ secret * 64) % 16777216;
 	secret = (secret / 32 ^ secret) % 16777216;
@@ -40,9 +57,42 @@ void solution(std::string file) {
 	Utils::copy(total);
 
 	// Part 2
+	std::set<Sequence> allSequences;
+	std::vector<std::map<Sequence, int>> options;
+	for (int i = 0; i < lines.size(); i++) {
+		long long secret = std::stoll(lines[i]);
+		std::vector<int> prices = { static_cast<int>(secret % 10) };
+		std::vector<int> differences;
+		options.push_back(std::map<Sequence, int>());
+		for (int j = 0; j < 2000; j++) {
+			secret = advanceSecret(secret);
+			prices.push_back(secret % 10);
+			differences.push_back(prices[prices.size() - 1] - prices[prices.size() - 2]);
+			if (differences.size() >= 4) {
+				Sequence sequence = { differences[differences.size() - 4], differences[differences.size() - 3], differences[differences.size() - 2], differences[differences.size() - 1] };
+				allSequences.insert(sequence);
+				if (!options[i].count(sequence)) {
+					options[i][sequence] = prices[prices.size() - 1];
+				}
+			}
+		}
+	}
 
-	//std::cout << "Part 2: " <<  << "\n";
-	//Utils::copy();
+	long long best = 0;
+	Sequence bestSequence = { 0, 0, 0, 0 };
+	for (const Sequence& sequence : allSequences) {
+		long long total = 0;
+		for (int i = 0; i < lines.size(); i++) {
+			if (options[i].count(sequence)) total += options[i][sequence];
+		}
+		if (total > best) {
+			best = total;
+			bestSequence = sequence;
+		}
+	}
+
+	std::cout << "Part 2: " << best << "\n";
+	Utils::copy(best);
 }
 
 int main() {
