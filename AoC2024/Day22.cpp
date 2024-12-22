41d3ba47f296b3d271adb208667978971d9a5158
LochlainnB <golden.github@brindley.it>
2024-12-22 20:49:15 +0000

    Finished Day 22 Part 1.

--- AoC2024/Day22.cpp
+++ AoC2024/Day22.cpp
@@ -9,6 +9,13 @@
 #include <set>
 #include <queue>
 
+long long advanceSecret(long long secret) {
+	secret = (secret ^ secret * 64) % 16777216;
+	secret = (secret / 32 ^ secret) % 16777216;
+	secret = (secret ^ secret * 2048) % 16777216;
+	return secret;
+}
+
 void solution(std::string file) {
 	std::string rawInput = Utils::loadFile(file);
 	// Use lines for 1D vector
@@ -20,9 +27,17 @@ void solution(std::string file) {
 	}
 
 	// Part 1
+	long long total = 0;
+	for (int i = 0; i < lines.size(); i++) {
+		long long secret = std::stoll(lines[i]);
+		for (int j = 0; j < 2000; j++) {
+			secret = advanceSecret(secret);
+		}
+		total += secret;
+	}
 
-	std::cout << "Part 1: " <<  << "\n";
-	Utils::copy();
+	std::cout << "Part 1: " << total << "\n";
+	Utils::copy(total);
 
 	// Part 2
 
