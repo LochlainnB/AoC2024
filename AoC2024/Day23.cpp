6bc4210eea0ed88850a958fa2660fbf1fe49a917
LochlainnB <golden.github@brindley.it>
2024-12-23 23:46:02 +0000

    Finished Day 23 Part 2.

--- AoC2024/Day23.cpp
+++ AoC2024/Day23.cpp
@@ -12,6 +12,61 @@
 struct Computer {
 	std::string identifier;
 	std::map<std::string, Computer*> connections;
+
+	std::vector<std::string> getLargestNetwork(int targetSize) {
+		std::vector<std::string> result;
+		for (int i = targetSize; i > 0; i--) {
+			std::vector<std::map<std::string, Computer*>::iterator> iterators;
+			result = recursiveThing(connections.begin(), i - 1, iterators);
+			if (result.size() > 0) {
+				result.insert(result.begin(), identifier);
+				return result;
+			}
+		}
+		return result;
+	}
+
+	std::vector<std::string> recursiveThing(std::map<std::string, Computer*>::iterator start, int remainingLevels, std::vector<std::map<std::string, Computer*>::iterator>& iterators) {
+		for (auto it = start; it != connections.end(); it++) {
+			bool inNetwork = true;
+			for (auto higherIt : iterators) {
+				if (!higherIt->second->connections.count(it->first)) {
+					inNetwork = false;
+				}
+			}
+			if (inNetwork) {
+				if (remainingLevels == 0) {
+					std::vector<std::string> result;
+					for (auto higherIt : iterators) {
+						result.push_back(higherIt->first);
+					}
+					result.push_back(it->first);
+					return result;
+				}
+				else {
+					auto nextIt = it;
+					nextIt++;
+					iterators.push_back(it);
+					std::vector<std::string> result = recursiveThing(nextIt, remainingLevels - 1, iterators);
+					iterators.erase(iterators.end() - 1);
+					if (result.size() > 0) return result;
+				}
+			}
+		}
+		return {};
+	}
+};
+
+struct SortName {
+	bool operator()(const Computer* lhs, const Computer* rhs) {
+		return lhs->identifier < rhs->identifier;
+	}
+};
+
+struct SortSize {
+	bool operator()(const Computer* lhs, const Computer* rhs) {
+		return lhs->connections.size() > rhs->connections.size();
+	}
 };
 
 void solution(std::string file) {
@@ -55,8 +110,31 @@ void solution(std::string file) {
 
 	// Part 2
 
-	//std::cout << "Part 2: " <<  << "\n";
-	//Utils::copy();
+	std::vector<Computer*> computersSorted;
+	for (auto it = computers.begin(); it != computers.end(); it++) {
+		computersSorted.push_back(it->second);
+	}
+	std::sort(computersSorted.begin(), computersSorted.end(), SortSize());
+
+	std::vector<std::string> lanNetwork;
+	for (int i = 0; i < computersSorted.size(); i++) {
+		if (lanNetwork.size() >= computersSorted[i]->connections.size()) {
+			break;
+		}
+		std::vector<std::string> candidateNetwork = computersSorted[i]->getLargestNetwork(computersSorted[i]->connections.size());
+		if (candidateNetwork.size() > lanNetwork.size()) {
+			lanNetwork = candidateNetwork;
+		}
+	}
+
+	std::string password = "";
+	for (int i = 0; i < lanNetwork.size(); i++) {
+		password += lanNetwork[i] + ",";
+	}
+	password.erase(password.begin() + password.size() - 1);
+
+	std::cout << "Part 2: " << password << "\n";
+	Utils::copy(password);
 }
 
 int main() {
