a9fac49dad7b4d2091f8d50fac4904dd91021168
LochlainnB <golden.github@brindley.it>
2024-12-23 21:45:58 +0000

    Finished Day 23 Part 1.

--- AoC2024/Day23.cpp
+++ AoC2024/Day23.cpp
@@ -9,6 +9,11 @@
 #include <set>
 #include <queue>
 
+struct Computer {
+	std::string identifier;
+	std::map<std::string, Computer*> connections;
+};
+
 void solution(std::string file) {
 	std::string rawInput = Utils::loadFile(file);
 	// Use lines for 1D vector
@@ -16,13 +21,37 @@ void solution(std::string file) {
 	// Use input for 2D vector
 	std::vector<std::vector<std::string>> input;
 	for (int i = 0; i < lines.size(); i++) {
-		input.push_back(Utils::split(lines[i], " "));
+		input.push_back(Utils::split(lines[i], "-"));
 	}
 
 	// Part 1
+	std::map<std::string, Computer*> computers;
+	for (int i = 0; i < input.size(); i++) {
+		for (int j = 0; j < input[i].size(); j++) {
+			if (!computers.count(input[i][j])) {
+				computers[input[i][j]] = new Computer{ input[i][j] };
+			}
+		}
+		computers[input[i][0]]->connections[input[i][1]] = computers[input[i][1]];
+		computers[input[i][1]]->connections[input[i][0]] = computers[input[i][0]];
+	}
 
-	std::cout << "Part 1: " <<  << "\n";
-	Utils::copy();
+	long long total = 0;
+	for (auto it1 = computers.begin(); it1 != computers.end(); it1++) {
+		for (auto it2 = it1; it2 != computers.end(); it2++) {
+			for (auto it3 = it2; it3 != computers.end(); it3++) {
+				if (it1->second->connections.count(it2->first)
+				&& it1->second->connections.count(it3->first)
+				&& it2->second->connections.count(it3->first)
+				&& (it1->first[0] == 't' || it2->first[0] == 't' || it3->first[0] == 't')) {
+					total++;
+				}
+			}
+		}
+	}
+
+	std::cout << "Part 1: " << total << "\n";
+	Utils::copy(total);
 
 	// Part 2
 
